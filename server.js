bc22309874d497b0a6ce853881051efd9f821d33
Dragiinus <melvinmaute@gmail.com>
2023-07-18 15:14:16 +0200

    Fichier de connexion à la BDD
    Connexion à la BDD avec Sequelize

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-/**********************************/
-/* Import des modules necessaires */
-/**********************************/
+/**************************************/
+/*** Import des modules necessaires ***/
+/**************************************/
 const express = require("express");
 const cors = require('cors')
 
-/***************************/
-/* Initialisation de l'API */
-/***************************/
+/*******************************/
+/*** Initialisation de l'API ***/
+/*******************************/
 const app = express();
 
 app.use(cors())
@@ -14,17 +14,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 
-/****************************/
-/* Mise en place du routage */
-/****************************/
+/********************************/
+/*** Mise en place du routage ***/
+/********************************/
 app.get("/", (req, res) => res.send(`I'm online, well done !`))
 
 app.get("*", (req, res) => res.status(501).send(`This ressource doesn't exist`))
 
 
-/****************/
-/* Start server */
-/****************/
+/********************/
+/*** Start server ***/
+/********************/
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
 })
