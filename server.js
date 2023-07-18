6ecb521f6b12f3232a83794cac70441db80b0504
Dragiinus <melvinmaute@gmail.com>
2023-07-18 14:58:06 +0200

    Mise en place
    Mise en place du backend

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
+/**********************************/
+/* Import des modules necessaires */
+/**********************************/
 const express = require("express");
-const port = 5000;
+const cors = require('cors')
 
+/***************************/
+/* Initialisation de l'API */
+/***************************/
 const app = express();
 
-app.get("/post", (req, res) => {
-    res.json({ message: "Voici les données"})
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+
+/****************************/
+/* Mise en place du routage */
+/****************************/
+app.get("/", (req, res) => res.send(`I'm online, well done !`))
+
+app.get("*", (req, res) => res.status(501).send(`This ressource doesn't exist`))
+
+
+/****************/
+/* Start server */
+/****************/
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
 })
-
-
-/*********************/
-/* Lancer le serveur */
-/*********************/
-app.listen(port, () => console.log("Le serveur a démarré au port " + port));
