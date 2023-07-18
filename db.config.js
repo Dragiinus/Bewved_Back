8bc636b37e2cb4d0ed3525fd31ee4251a4627999
Dragiinus <melvinmaute@gmail.com>
2023-07-18 15:37:23 +0200

    Connexion à la BDD

--- db.config.js
+++ db.config.js
@@ -8,8 +8,8 @@ const { Sequelize } = require("sequelize")
 /**************************************/
 let sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: DB_HOST,
-        port: DB_PORT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: 'mysql',
         logging: false
     }
--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 const express = require("express");
 const cors = require('cors')
 
+/********************************************/
+/*** Import de la connexion à la database ***/
+/********************************************/
+let DB = require("./db.config")
+
 /*******************************/
 /*** Initialisation de l'API ***/
 /*******************************/
@@ -22,9 +27,15 @@ app.get("/", (req, res) => res.send(`I'm online, well done !`))
 app.get("*", (req, res) => res.status(501).send(`This ressource doesn't exist`))
 
 
-/********************/
-/*** Start server ***/
-/********************/
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
-})
+/***************************************/
+/*** Start server avec test database ***/
+/***************************************/
+DB.authenticate()
+    .then(() => console.log("Database connection OK"))
+    .then(() => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
+        })
+    })
+    .catch(err => console.log("Database Error", err))
+
