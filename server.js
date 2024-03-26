7281634f5581495f4c2d50556fb018016dbad400
Dragiinus <melvinmaute@gmail.com>
2024-03-26 20:40:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /**************************************/
 const express = require("express");
 const cors = require("cors");
-const api = "/api/v0/";
+const api = "/api";
 
 /********************************************/
 /*** Import de la connexion à la database ***/
